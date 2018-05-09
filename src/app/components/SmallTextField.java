ef5364d0d6a928f10e8c2849c99a924929739f67
Marcel Ebert <marcel@satoshipay.io>
2018-05-09 13:44:40 +0200

    changed editorpane

---
+++ src/app/components/SmallTextField.java
@@ -0,0 +1,20 @@
+package app.components;
+
+import javafx.scene.control.TextField;
+
+public class SmallTextField extends TextField {
+
+    private static final int PREF_WIDTH = 70;
+    private static final int PREF_HEIGHT = 20;
+
+    public SmallTextField() {
+        setPrefWidth(PREF_WIDTH);
+        setPrefHeight(PREF_HEIGHT);
+    }
+
+    public SmallTextField(String text) {
+        super(text);
+        setPrefWidth(PREF_WIDTH);
+        setPrefHeight(PREF_HEIGHT);
+    }
+}
--- src/app/pane/EditorPane.java
+++ src/app/pane/EditorPane.java
@@ -1,5 +1,6 @@
 package app.pane;
 
+import app.components.SmallTextField;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
@@ -12,15 +13,19 @@ import javafx.scene.layout.GridPane;
 
 public class EditorPane extends ScrollPane {
 
+    private static final int COUNT_OF_STANDARD_ITEMS_PER_ROW = 8;
+
     static final ObservableList typeList = FXCollections.observableArrayList(
             "Polygon", "Line", "Point", "Text");
     static final ObservableList fontFamilyList = FXCollections.observableArrayList(
             "Times New Roman", "Arial");
-    static final ObservableList fontStyleList = FXCollections.observableArrayList(
+    static final ObservableList fontWeightList = FXCollections.observableArrayList(
             "Normal", "Bold");
 
     static final ObservableList<Integer> zoomList = FXCollections.observableArrayList(
-            1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13);
+            0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16);
+
+    // TODO min max zoom und filter vor andere dinger schieben, weil nur 1 pro regel
 
     public EditorPane() {
         super();
@@ -45,27 +50,12 @@ public class EditorPane extends ScrollPane {
         pane.setVgap(10);
         pane.setHgap(10);
         // create name components
-        Label nameLabel = new Label("NamedLayerName");
+        Label nameLabel = new Label("Name");
         TextField nameTextField = new TextField();
-        // create title components
-        Label titleLabel = new Label("UserStyleTitle");
-        TextField titleTextField = new TextField();
-        // create sldname components
-        Label sldNameLabel = new Label("UserStyleAbstract");
-        TextField sldNameTextField = new TextField();
-        // create sldliteral components
-        Label sldLiteralLabel = new Label("Rule Name");
-        TextField sldLiteralTextField = new TextField();
 
 
         pane.add(nameLabel, 0, 0);
         pane.add(nameTextField, 1, 0);
-        pane.add(titleLabel, 2, 0);
-        pane.add(titleTextField, 3, 0);
-        pane.add(sldNameLabel, 0, 1);
-        pane.add(sldNameTextField, 1, 1);
-        pane.add(sldLiteralLabel, 2, 1);
-        pane.add(sldLiteralTextField, 3, 1);
 
         return pane;
     }
@@ -75,23 +65,18 @@ public class EditorPane extends ScrollPane {
 
         pane.setPadding(new Insets(20, 20, 20, 20));
         pane.setHgap(10);
+        pane.setVgap(5);
 
         Label typeLabel = new Label("Typ");
         ChoiceBox typeBox = new ChoiceBox(typeList);
 
-        Label zoomLabel = new Label("Zoom");
-        ChoiceBox zoomBox = new ChoiceBox(zoomList);
+        Label minZoomLabel = new Label("min Zoom");
+        ChoiceBox minZoomBox = new ChoiceBox(zoomList);
+        Label maxZoomLabel = new Label("max Zoom");
+        ChoiceBox maxZoomBox = new ChoiceBox(zoomList);
 
-        Label fillLabel = new Label("Füllung");
-        TextField fillTextField = new TextField("#000000");
-
-
-        // text stuff
-        Label fontFamilyLabel = new Label("Schriftart");
-        ChoiceBox fontFamilyBox = new ChoiceBox(fontFamilyList);
-        Label fontStyleLabel = new Label("Schriftstil");
-        ChoiceBox fontStyleBox = new ChoiceBox(fontStyleList);
-
+        Label filterLabel = new Label("Filter");
+        SmallTextField filterTextField = new SmallTextField("");
 
         // add listener to typeBox after components have been initialized
         typeBox.getSelectionModel().selectedItemProperty()
@@ -101,22 +86,16 @@ public class EditorPane extends ScrollPane {
                         // remove unnecessary components based in oldvalue
                         switch (value) {
                             case "Polygon":
-                                pane.getChildren().remove(fillLabel);
-                                pane.getChildren().remove(fillTextField);
+                                removeAdditionalItemsFrom(pane);
                                 break;
                             case "Point":
-                                pane.getChildren().remove(fillLabel);
-                                pane.getChildren().remove(fillTextField);
+                                removeAdditionalItemsFrom(pane);
                                 break;
                             case "Line":
-                                pane.getChildren().remove(fillLabel);
-                                pane.getChildren().remove(fillTextField);
+                                removeAdditionalItemsFrom(pane);
                                 break;
                             case "Text":
-                                pane.getChildren().remove(fontFamilyBox);
-                                pane.getChildren().remove(fontFamilyLabel);
-                                pane.getChildren().remove(fontStyleBox);
-                                pane.getChildren().remove(fontStyleLabel);
+                                removeAdditionalItemsFrom(pane);
                                 break;
                         }
                     }
@@ -125,23 +104,17 @@ public class EditorPane extends ScrollPane {
                     // add components based on new value
                     switch (value) {
                         case "Polygon":
-                            addFillItemsToPane(pane, fillLabel, fillTextField);
+                            addPolygonItemsTo(pane);
 
                             break;
                         case "Point":
-                            addFillItemsToPane(pane, fillLabel, fillTextField);
-
+                            addPointItemsTo(pane);
                             break;
                         case "Line":
-                            addFillItemsToPane(pane, fillLabel, fillTextField);
 
                             break;
                         case "Text":
-                            pane.add(fontFamilyLabel, 6, 0);
-                            pane.add(fontFamilyBox, 7, 0);
-
-                            pane.add(fontStyleLabel, 8, 0);
-                            pane.add(fontStyleBox, 9, 0);
+                            addTextItemsTo(pane);
 
                             break;
                     }
@@ -150,16 +123,109 @@ public class EditorPane extends ScrollPane {
 
         pane.add(typeLabel, 0, 0);
         pane.add(typeBox, 1, 0);
-        pane.add(zoomLabel, 2, 0);
-        pane.add(zoomBox, 3, 0);
+        pane.add(minZoomLabel, 2, 0);
+        pane.add(minZoomBox, 3, 0);
+        pane.add(maxZoomLabel, 4, 0);
+        pane.add(maxZoomBox, 5, 0);
+        pane.add(filterLabel, 6, 0);
+        pane.add(filterTextField, 7, 0);
 
         return pane;
     }
 
-    // extra method so position in row has to be changed in only one place -> here
-    private void addFillItemsToPane(GridPane pane, Label fillLabel, TextField fillTextField) {
-        pane.add(fillLabel, 4, 0);
-        pane.add(fillTextField, 5, 0);
+    private void addPolygonItemsTo(GridPane pane) {
+        Label fillLabel = new Label("Füllfarbe");
+        SmallTextField fillText = new SmallTextField("#000000");
+
+        Label fillOpacityLabel = new Label("Deckkraft der Farbe");
+        SmallTextField fillOpacityText = new SmallTextField("1.0");
+
+        Label strokeLabel = new Label("Strichfarbe");
+        SmallTextField strokeText = new SmallTextField("#000000");
+
+        Label strokeWidthLabel = new Label("Dicke des Striches");
+        SmallTextField strokeWidthText = new SmallTextField("1.0");
+
+        Label strokeDashLabel = new Label("Strich-Muster");
+        SmallTextField strokeDashText = new SmallTextField("5 2");
+
+        pane.add(fillLabel, 2, 1);
+        pane.add(fillText, 3, 1);
+        pane.add(fillOpacityLabel, 4, 1);
+        pane.add(fillOpacityText, 5, 1);
+        pane.add(strokeLabel, 2, 2);
+        pane.add(strokeText, 3, 2);
+        pane.add(strokeWidthLabel, 4, 2);
+        pane.add(strokeWidthText, 5, 2);
+        pane.add(strokeDashLabel, 6, 2);
+        pane.add(strokeDashText, 7, 2);
+    }
+
+    private void removeAdditionalItemsFrom(GridPane pane) {
+        pane.getChildren().remove(COUNT_OF_STANDARD_ITEMS_PER_ROW, pane.getChildren().size());
+
+    }
+
+    private void addPointItemsTo(GridPane pane) {
+        Label graphicNameLabel = new Label("Form");
+        SmallTextField graphicNameText = new SmallTextField("circle");
+
+        Label fillLabel = new Label("Füllfarbe");
+        SmallTextField fillText = new SmallTextField("#000000");
+
+        Label sizeLabel = new Label("Größe");
+        SmallTextField sizeText = new SmallTextField("3");
+
+        pane.add(graphicNameLabel, 2, 1);
+        pane.add(graphicNameText, 3, 1);
+        pane.add(fillLabel, 4, 1);
+        pane.add(fillText, 5, 1);
+        pane.add(sizeLabel, 6, 1);
+        pane.add(sizeText, 7, 1);
+    }
+
+    private void addTextItemsTo(GridPane pane) {
+        Label labelLabel = new Label("Label");
+        TextField labelText = new SmallTextField("");
+
+        Label fontSizeLabel = new Label("Schriftgröße");
+        TextField fontSizeText = new SmallTextField("12");
+
+        Label fontWeightLabel = new Label("Schriftstil");
+        ChoiceBox fontWeightBox = new ChoiceBox(fontWeightList);
+
+        Label fillLabel = new Label("Farbe");
+        TextField fillText = new SmallTextField("#000000");
+
+        Label anchorXLabel = new Label("Ankerpunkt X");
+        TextField anchorXText = new SmallTextField("0");
+
+        Label anchorYLabel = new Label("Ankerpunkt Y");
+        TextField anchorYText = new SmallTextField("0");
+
+        Label displacementXLabel = new Label("Verschiebung X");
+        TextField displacementXText = new SmallTextField("0");
+
+        Label displacementYLabel = new Label("Verschiebung Y");
+        TextField displacementYText = new SmallTextField("0");
+
+        pane.add(labelLabel, 2,1);
+        pane.add(labelText, 3,1);
+        pane.add(fontSizeLabel, 4,1);
+        pane.add(fontSizeText, 5,1);
+        pane.add(fontWeightLabel, 6,1);
+        pane.add(fontWeightBox, 7,1);
+        pane.add(fillLabel, 8,1);
+        pane.add(fillText, 9,1);
+        pane.add(anchorXLabel, 2,2);
+        pane.add(anchorXText, 3,2);
+        pane.add(anchorYLabel, 4,2);
+        pane.add(anchorYText, 5,2);
+        pane.add(displacementXLabel, 6,2);
+        pane.add(displacementXText, 7,2);
+        pane.add(displacementYLabel, 8,2);
+        pane.add(displacementYText, 9,2);
+
     }
 
 }
